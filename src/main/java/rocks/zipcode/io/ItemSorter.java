7e0a376409dd5a4a3501631775e2c059422e3322
Aswathy Kanakarajan <aswathykanakarajan@zipcoders-MacBook-Pro-7.local>
2019-02-20 15:01:46 -0500

    Implemented / tested - All testcase passed

--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -1,15 +1,21 @@
 package rocks.zipcode.io;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 
 /**
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+    ArrayList<Item> items;
     public ItemSorter(Item[] items) {
+        this.items = new ArrayList<Item>(Arrays.asList(items));
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        return null;
+
+        items.sort(comparator);
+        return items.toArray(new Item[items.size()]);
     }
 }
--- src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
@@ -1,7 +1,18 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class PriceComparator {
+public class PriceComparator implements Comparator<Item> {
+    public int compare(Item o1, Item o2) {
+        double result = (o1.getPrice() - o2.getPrice());
+        int returnValue = result > 0 ? 1: -1;
+        if(result == 0)
+            returnValue = 0;
+        return returnValue;
+    }
 }
